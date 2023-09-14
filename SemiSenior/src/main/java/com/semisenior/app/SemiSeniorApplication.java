8184addad42ceaf76008de87bbb1a3a4e08cc94f
JavierCostas81 <fercostas81@gmail.com>
2023-09-14 18:20:01 -0300

    Proyecto Item-Producto de Udemy

---
+++ SemiSenior/src/main/java/com/semisenior/app/SemiSeniorApplication.java
@@ -0,0 +1,13 @@
+package com.semisenior.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemiSeniorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemiSeniorApplication.class, args);
+	}
+
+}
---
+++ SemiSenior/src/test/java/com/semisenior/app/SemiSeniorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semisenior.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemiSeniorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
